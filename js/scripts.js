e805401e45322d8bc24020bf758131104aa7c35a
Desmond Williams <demowi8@gmail.com>
2021-08-05 02:34:11 -0700

    Working Porfolio

--- js/scripts.js
+++ js/scripts.js
@@ -7,6 +7,7 @@ window.addEventListener('DOMContentLoaded', event => {
 
     const sidebarWrapper = document.getElementById('sidebar-wrapper');
     let scrollToTopVisible = false;
+    
     // Closes the sidebar menu
     const menuToggle = document.body.querySelector('.menu-toggle');
     menuToggle.addEventListener('click', event => {
